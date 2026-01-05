867c27ab46aac36c9fe8e9868a0edb54a2c488dc
faeck-the-developer <madduxsoanes@gmail.com>
2026-01-05 12:46:49 -0500

    Add address formatter program
    This program formats and prints user contact information.

---
+++ address_formatter.py
@@ -0,0 +1,11 @@
+# Job: This program takes user contact info and prints it in a standard address format.
+# Input: Four strings (First name, Last name, Address, City/PC)
+
+firstname = input("Given name: ")
+familyname = input("Family name: ")
+address = input("Street address: ")
+citypc = input("City and postal code: ")
+
+print(firstname + " " + familyname)
+print(address) 
+print(citypc)
