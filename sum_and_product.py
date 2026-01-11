0e16eb8e47a1b43512b5e91cc9b0bb3ecbf12331
faeck-the-developer <madduxsoanes@gmail.com>
2026-01-10 21:28:51 -0500

    Implemented a program to calculate both sum and product from user input using f-strings.
    Implement a program to calculate sum and product of two numbers.

---
+++ sum_and_product.py
@@ -0,0 +1,15 @@
+# Please write a program which asks the user for two numbers. The program will then print out the sum and the product of the two numbers. The program should function as follows:
+Number 1: 3
+Number 2: 7
+The sum of the numbers: 10
+The product of the numbers: 21
+
+num1 = int(input ("Number 1: "))
+num2 = int(input ("Number 2: "))
+result = num1 + num2
+result2 = num1 * num2
+print (f"The sum of the numbers: {result}")
+print (f"The product of the numbers: {result2}")
+
+
+
