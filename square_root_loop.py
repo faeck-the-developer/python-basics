5d776db046f964a21788acb5580d98821fc75757
faeck-the-developer <madduxsoanes@gmail.com>
2026-01-29 20:40:46 -0500

    feat: square root calculator with loop and negative number handling

---
+++ square_root_loop.py
@@ -0,0 +1,7 @@
+if num > 0:
+        print(sqrt(num))
+    elif num < 0:
+        print("Invalid number")
+    else: # If it's not > 0 and not < 0, it MUST be 0
+        print("Exiting...")
+        break
